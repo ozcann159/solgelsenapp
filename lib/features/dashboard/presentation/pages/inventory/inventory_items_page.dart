83f1edfe7340b27faaeebc1f603a2d16080a5798
Elif Özcan <ozcann159@gmail.com>
2025-04-16 12:39:41 +0300

    create inventory items page

--- lib/features/dashboard/presentation/pages/inventory/inventory_items_page.dart
+++ lib/features/dashboard/presentation/pages/inventory/inventory_items_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:solgensenapp/core/constants/app_colors.dart';
 
 class InventoryItemsPage extends StatefulWidget {
   const InventoryItemsPage({super.key});
@@ -8,8 +9,52 @@ class InventoryItemsPage extends StatefulWidget {
 }
 
 class _InventoryItemsPageState extends State<InventoryItemsPage> {
+  bool isLoading = false;
+  String? selectedCategoryId;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Inventory Item List',
+            style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: AppColors.primary,
+          elevation: 0,
+          leading: Builder(builder: (context) {
+            return IconButton(
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+                size: 27,
+              ),
+              onPressed: () {
+                if (Navigator.canPop(context)) {
+                  Navigator.pop(context);
+                } else {
+                  Scaffold.of(context).openDrawer();
+                }
+              },
+            );
+          }),
+          // actions: [
+          //   IconButton(
+          //     onPressed: () {
+
+          //     },
+          //     icon: Icon(Icons.refresh),
+          //   ),
+          // ], // refresh button
+        ),
+        body: Card(
+          margin: EdgeInsets.only(bottom: 8),
+          child: ListTile(),
+        ));
   }
 }
