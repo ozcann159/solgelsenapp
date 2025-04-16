27320971edd5b4e3d3a65d98f68b943d9e74ef3d
Elif Özcan <ozcann159@gmail.com>
2025-04-16 17:27:19 +0300

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:solgensenapp/core/theme/app_theme.dart';
-<<<<<<< HEAD
-import 'package:solgensenapp/features/dashboard/presentation/pages/account_list_page.dart';
-import 'package:solgensenapp/features/dashboard/presentation/pages/home_page.dart';
-=======
->>>>>>> elif
 import 'package:solgensenapp/features/splash/splash_page.dart';
 import 'package:solgensenapp/firebase_options.dart';
 
@@ -23,11 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SİBERTRONİK',
       theme: AppTheme.lightTheme,
-<<<<<<< HEAD
-      home: const AccountListPage(),
-=======
       home: const SplashPage(),
->>>>>>> elif
       //SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
