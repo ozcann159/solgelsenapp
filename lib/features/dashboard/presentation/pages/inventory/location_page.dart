bf05910efdcedc8f52f10f8e5e15c8ebadd8c4fb
Elif Özcan <ozcann159@gmail.com>
2025-04-16 17:20:42 +0300

    feat: warehouse page, services, firebase add

--- lib/features/dashboard/presentation/pages/inventory/location_page.dart
+++ lib/features/dashboard/presentation/pages/inventory/location_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:solgensenapp/core/constants/app_colors.dart';
 
 class LocationPage extends StatefulWidget {
   const LocationPage({super.key});
@@ -10,6 +11,56 @@ class LocationPage extends StatefulWidget {
 class _LocationPagegState extends State<LocationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xFFF5f5f5),
+      // appBar: AppBar(
+      //   title: const Text(
+      //     'Supplier List',
+      //     style: TextStyle(
+      //       fontFamily: 'Montserrat',
+      //       fontSize: 18,
+      //       fontWeight: FontWeight.bold,
+      //       color: Colors.white,
+      //     ),
+      //   ),
+      //   backgroundColor: AppColors.primary,
+      //   elevation: 0,
+      //   leading: Builder(
+      //     builder: (context) {
+      //       return IconButton(
+      //         icon: const Icon(
+      //           Icons.arrow_back,
+      //           color: Colors.white,
+      //           size: 27,
+      //         ),
+      //         onPressed: () {
+      //           if (Navigator.canPop(context)) {
+      //             Navigator.pop(context);
+      //           } else {
+      //             Scaffold.of(context).openDrawer();
+      //           }
+      //         },
+      //       );
+      //     },
+      //   ),
+      //   actions: [
+          
+      //     IconButton(
+      //       icon: const Icon(
+      //         Icons.refresh,
+      //         color: Colors.white,
+      //       ),
+      //       onPressed: _loadData,
+      //     ),
+      //     IconButton(
+      //       icon: const Icon(
+      //         Icons.delete,
+      //         color: Colors.white,
+      //       ),
+      //       onPressed: selectedSuppliers.isEmpty ? null : _deleteSelected,
+      //     ),
+      //   ],
+      // ),
+    );
   }
 }
